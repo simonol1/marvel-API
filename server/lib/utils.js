b67a4e7431ae4f13914bba87748fcc02e5c26e72
simonol1 <simondanolearyis@gmail.com>
2017-10-15 21:05:39 +1000

    added components and corrected api, but not functional yet

--- server/lib/utils.js
+++ server/lib/utils.js
@@ -6,7 +6,7 @@ function getTimeStamp() {
 
 function createHash (timestamp, privateKey, publicKey) {
   var preHash = timestamp + privateKey + publicKey
-    , hash    = crypto.createHash('md5'),update(preHash).digest('hex');
+    , hash    = crypto.createHash('md5').update(preHash).digest('hex');
   return hash;
 }
 
