3ff968806b495776bd329ad7d4003c9ebbef4eb7
simonol1 <simondanolearyis@gmail.com>
2017-10-15 14:32:42 +1000

    added server routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 
-var greetings = require('./routes/greeting')
+var characters = require('./routes/characters')
 
 var server = express()
 
@@ -12,7 +12,7 @@ server.use(cors('*'))
 server.use(bodyParser.json())
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/greetings', greetings)
+server.use('/api/v1', characters)
 
 module.exports = function(db) {
   server.set('db', db)
