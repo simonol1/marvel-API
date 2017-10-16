8ac7611dce09bf403279aa53f481bc522a8aab0d
Simon O'Leary <simono@benon.com>
2017-10-16 16:21:43 +1000

    2 tests not passing yet

--- test/client/index.test.js
+++ test/client/index.test.js
@@ -7,7 +7,13 @@ import './setup-dom'
 
 App.prototype.componentDidMount = () => {}
 
-test('The title renders on the page, t => {
+test('The title image renders on the page', t => {
+  const wrapper = mount(<App />)
+  console.log(wrapper.html())
+  t.is(wrapper.find('.header_image img[src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqmOQXn9ai3iH3IcyHW7bHPmaBUF6AI_HLZWFXJZ3_kF94CpN-"]').exists(), true)
+})
+
+test('The footer renders on the page', t=> {
   const wrapper = shallow(<App />)
-  t.is(wrapper.find('h1').text(), 'Marvel')
+  t.is(wrapper.find('p').text(), 'Data provided by Marvel')
 })
